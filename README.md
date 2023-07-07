b500b026f1646eefbf29c26907f6435a90f17517
cgmccarron <mccarron.christian13@gmail.com>
2023-07-07 17:20:00 -0500

    added icons to header section

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# odinbook
+# Odinbook
 
 Odin Project's final project. This is a facebook clone using the MERN stack which consist of MongoDB, Express, React, and NodeJS.
 
-[MongoDB](https://www.mongodb.com/) as the database.
-[Express](https://expressjs.com/) as the sever framework in javascript.
-[React](https://react.dev/) is the UI framework in javascript.
+[MongoDB](https://www.mongodb.com/) as the database. <br>
+[Express](https://expressjs.com/) as the sever framework in javascript. <br>
+[React](https://react.dev/) is the UI framework in javascript.<br>
 [Node.js](https://nodejs.org/en) is the javascript runtime environment.
 
 For the UI component and icon library I used [Material UI](https://mui.com/).
 
 Author: [Christian McCarron](https://github.com/cgmccarron)
 
-## avaliable scripts
+### avaliable scripts
 
 In the project directory, you can run the following commands:
 
-To start the client:
-
-### `cd client; npm start`
+To start the client:  
+`cd client; npm start`
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,31 @@
 import React from "react";
 import {
-  Home,
+  HomeOutlined as Home,
   Search,
-  Flag,
-  SubscriptionsOutlined,
-  SupervisedUserCircleOutlined,
+  FlagOutlined as Flag,
+  SubscriptionsOutlined as Subscriptions,
+  SupervisedUserCircleOutlined as Users,
+  StorefrontOutlined as Storefront,
+  AccountBox as Avatar,
+  Add,
+  Forum,
+  NotificationsActive,
+  ExpandMore as Expand,
 } from "@mui/icons-material";
 
 const Header = () => {
   return (
     <div className="header">
+      {/* Left side of header to display logo */}
       <div className="header__left">
         <img src="" alt="Odinbook logo" />
       </div>
+      {/* searchbar for app*/}
       <div className="header__input">
         <Search />
         <input type="text" placeholder="Search" />
       </div>
+      {/* collapseable buttons for navigating the app */}
       <div className="header__center">
         <div className="header__option header__option__active">
           <Home fontSize="large" />
@@ -25,10 +34,24 @@ const Header = () => {
           <Flag fontSize="large" />
         </div>
         <div className="header__option">
-          <SubscriptionsOutlined fontSize="large" />
+          <Storefront fontSize="large" />
         </div>
         <div className="header__option">
-          <SupervisedUserCircleOutlined fontSize="large" />
+          <Subscriptions fontSize="large" />
+        </div>
+        <div className="header__option">
+          <Users fontSize="large" />
+        </div>
+      </div>
+      {/* right side of header to display avatar, user, and functions relating to the user */}
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar fontSize="large" />
+          <h4>Name</h4>
+          <Add />
+          <Forum />
+          <NotificationsActive />
+          <Expand />
         </div>
       </div>
     </div>
