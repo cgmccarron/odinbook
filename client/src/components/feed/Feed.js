8f330c2f1101dffc382beb117227bfb3b57a26c2
cgmccarron <mccarron.christian13@gmail.com>
2023-07-11 03:12:10 -0500

    added a post component

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,12 +1,20 @@
 import React from "react";
 import StoryReel from "./story/StoryReel";
 import PostMaker from "./postMaker/PostMaker";
+import Post from "./post/Post";
 
 const Feed = () => {
   return (
     <div className="feed">
       <StoryReel />
       <PostMaker />
+      <Post
+        profilePic="https://media.licdn.com/dms/image/C4D03AQH1yMD3NzYDDQ/profile-displayphoto-shrink_800_800/0/1517493780761?e=1694649600&v=beta&t=faLehq2SPhoNtDZi4suG6SvXNGEISrbdjIRw5mSyVXQ"
+        message="This is my message in the post"
+        timestamp="1689061292"
+        imgName="imgName"
+        username="Christian McCarron"
+      />
 
       {/*{postData.map((entry) => (
         <Post
---
+++ client/src/components/feed/post/Post.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Post.css";
+import { Avatar } from "@mui/material";
+import { ChatBubble, NearMe, ThumbUp } from "@mui/icons-material";
+
+const Post = ({ profilePic, username, imgName, message, timestamp }) => {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__info">
+          <h3>{username}</h3>
+          <p>{new Date(parseInt(timestamp)).toUTCString()}</p>
+        </div>
+      </div>
+      <div className="post__bottom">
+        <p>{message}</p>
+        {/* image name is coming later on */}
+      </div>
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUp />
+          <p>like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubble />
+          <p>comment</p>
+        </div>
+        <div className="post__option">
+          <NearMe />
+          <p>share</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
