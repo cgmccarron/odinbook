077a5b70d0e6f5d840134c5b6024471071bc31d4
cgmccarron <mccarron.christian13@gmail.com>
2023-07-11 17:13:44 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import bodyParser from "body-parser";
 import path from "path";
 import Pusher from "pusher";
 
+import mongoPosts from "./postModel.js";
+
 Grid.mongo = mongoose.mongo;
 
 //app config
@@ -20,12 +22,8 @@ app.use(cors());
 
 //db config
 const mongoURI =
-  "mongodb+srv://cgmccarr:Thunderup13!@cluster0.asa3mdm.mongodb.net/?retryWrites=true&w=majority";
-const conn = mongoose.createConnection(mongoURI, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  "mongodb+srv://cgmccarr:nwivkotrvMGYYfvf@cluster0.asa3mdm.mongodb.net/?retryWrites=true&w=majority";
+const conn = mongoose.createConnection(mongoURI);
 
 let gfs;
 
@@ -52,17 +50,55 @@ const storage = new GridFsStorage({
   },
 });
 
-mongoose.connect(mongoURI, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const upload = multer({ storage });
+
+mongoose.connect(mongoURI);
 
 // api routes
 app.get("/", (req, res) => {
   res.status(200).send("hello world");
 });
 
+app.post("./upload/images", upload.single("file"), (req, res) => {
+  res.status(201).send(req.file);
+});
+
+app.post("/upload/posts", (req, res) => {
+  const dbPost = req.body;
+
+  mongoPosts.create(dbPost, (err, posts) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      res.status(201).send(posts);
+    }
+  });
+});
+
+app.get("/retrieve/posts", (req, res) => {
+  mongoPosts.find((err, posts) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      posts.sort((a, b) => {
+        return b.timestamp - a.timestamp;
+      });
+
+      res.status(200).send(posts);
+    }
+  });
+});
+
+app.get("/retrieve/images/single", (req, res) => {
+  gfs.files.findOne({ filename: req.query.name }, (err, file) => {
+    if (err) {
+      res.status(404).json({ err: "file not found" });
+    } else {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    }
+  });
+});
 //listen
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
