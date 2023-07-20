b836d31a6a2aaa1bab2a9460b67cae4a39c0b83d
cgmccarron <mccarron.christian13@gmail.com>
2023-07-19 22:55:03 -0500

    added responsive styling to widget bar

--- client/src/components/widget/Widget.js
+++ client/src/components/widget/Widget.js
@@ -1,13 +1,14 @@
 import React from "react";
 
+import "./Widget.css";
+
 const Widget = () => {
   return (
     <div className="widget__container">
       <iframe
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftheodinproject%2F&tabs=timeline&width=340&height=1500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         style={{ border: "none", overflow: "hidden" }}
-        scrolling="no"
-        height="1500"
+        height="600"
         width="340"
         frameborder="0"
         allowfullscreen="true"
