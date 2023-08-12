be1e1bdbbbf6d4292dbae031b9bd45e9a606d806
cgmccarron <mccarron.christian13@gmail.com>
2023-08-12 17:24:05 -0500

    working on chat

--- client/src/pages/chat/Chat.js
+++ client/src/pages/chat/Chat.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Chat = () => {
-  return <div>This is the chat box</div>;
+  return <div>This is the chat box
+    Will need to have two users timestamp and message content
+    
+  </div>;
 };
 
 export default Chat;
